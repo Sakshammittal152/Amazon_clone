620d9c207d22b3c91fbde6b7e8f228164b0f8080
Sakshammittl <2023kucp1150@iiitkota.ac.in>
2026-05-23 19:48:21 +0530

    Api frontend url

--- client/src/api.js
+++ client/src/api.js
@@ -1,5 +1,6 @@
 const API_URL =
-  import.meta.env.VITE_API_URL || 'http://localhost:5000';
+  import.meta.env.VITE_API_URL ||
+  'http://localhost:5000/api';
 
 export function getToken() {
   return localStorage.getItem('token');
